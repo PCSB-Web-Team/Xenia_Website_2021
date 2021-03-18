2e45885cb82a74a72f1c6295b47315568aaea00e
daigavane70 <daigavane70@gmail.com>
2021-03-18 23:09:27 +0530

    web team Finalized

--- src/Components/WebTeam/TeamProfile.js
+++ src/Components/WebTeam/TeamProfile.js
@@ -9,10 +9,11 @@ const TeamProfile = ({details}) => {
           <div className='picNLink'>
             <img src={details.pic} className="memberPic"></img>
             <a className=" memberLink" href={details.linkedIn} target='_blank'>
-              <i className="footerIcon fa fa-3x fa-linkedin-square text-primary" aria-hidden="true" /> </a>
+              <i className="footerIcon fa fa-3x fa-linkedin-square text-primary" aria-hidden="true" /> 
+            </a>
           </div>
 
-          <p className='memberName'>{details.name}</p>
+          <div className='memberName'>{details.name}</div>
 
         </div>
     )
