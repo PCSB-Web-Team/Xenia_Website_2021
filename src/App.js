300aae4eaa69cccc2ec4559487a723b5e9c081b4
daigavane70 <daigavane70@gmail.com>
2021-03-22 17:31:22 +0530

    404 page added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Profile from './Components/Profile/Profile';
 import WebTeam from './Components/WebTeam/WebTeam';
 import Sponsors from './Components/Sponsors/Sponsors';
 import { getLoggedInUser } from './Components/Config/api/User';
+import NotFound from './Components/404/404';
 import './App.css';
 import Background from './Components/BackGround/Background';
 import {
@@ -44,51 +45,52 @@ const App = (props) => {
 			<Navbar />
 			<Background />
 			<Switch>
-				<Route path="/schedule">
+				<Route exact path="/schedule">
 					{' '}
 					<Schedule />
 					<Footer />{' '}
 				</Route>
-				<Route path="/events/:id">
+				<Route exact path="/events/:id">
 					{' '}
 					<EventDetail />
 					<Footer />{' '}
 				</Route>
-				<Route path="/events">
+				<Route exact path="/events">
 					{' '}
 					<Events />
 					<Footer />{' '}
 				</Route>
-				<Route path="/about-us">
+				<Route exact path="/about-us">
 					{' '}
 					<AboutUs />
 					<ContactUs />
 					<Footer />{' '}
 				</Route>
-				<Route path="/cart">
+				<Route exact path="/cart">
 					{' '}
 					<Cart />
 					<Footer />{' '}
 				</Route>
-				<Route path="/profile">
+				<Route exact path="/profile">
 					{' '}
 					<Profile />
 					<Footer />{' '}
 				</Route>
-				<Route path="/sponsors">
+				<Route exact path="/sponsors">
 					{' '}
 					<Sponsors />
 					<Footer />{' '}
 				</Route>
-				<Route path="/webteam">
+				<Route exact path="/webteam">
 					{' '}
 					<WebTeam />
 					<Footer />{' '}
 				</Route>
-				<Route path="/">
+				<Route exact path="/">
 					{' '}
 					<Home />{' '}
 				</Route>
+				<Route component={NotFound}/>
 				
 			</Switch>
 		</div>
