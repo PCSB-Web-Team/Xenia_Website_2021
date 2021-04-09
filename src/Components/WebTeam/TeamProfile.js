086e7c03fa41f5d675f4b8d272e1512a548ef81e
Amit Purohit <amitpurohit4747@gmail.com>
2021-04-09 08:32:23 +0530

    added target=_blank in web team

--- src/Components/WebTeam/TeamProfile.js
+++ src/Components/WebTeam/TeamProfile.js
@@ -8,7 +8,7 @@ const TeamProfile = ({details}) => {
             
           <div className='picNLink'>
             <img src={details.pic} className="memberPic" alt=''></img>
-            <a className=" memberLink" href={details.linkedIn}>
+            <a className=" memberLink" href={details.linkedIn} target="_blank">
               <i className="footerIcon fa fa-3x fa-linkedin-square text-primary" aria-hidden="true" /> 
             </a>
           </div>
