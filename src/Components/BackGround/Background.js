78d9e4e089c35b9309ba1a05d90e257d83076dae
daigavane70 <daigavane70@gmail.com>
2021-05-17 16:20:53 +0530

    cart logo fixed temporarily

--- src/Components/BackGround/Background.js
+++ src/Components/BackGround/Background.js
@@ -5,8 +5,8 @@ const Background = () => {
     return (
         <div className='mainBackground'>
             <div id='stars'></div>
-            <div id='stars2'></div>
-            <div id='stars3'></div>
+            {/* <div id='stars2'></div> */}
+            {/* <div id='stars3'></div> */}
         </div>
     )
 } 
