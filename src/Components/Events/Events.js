27932396a1fbddb21781da91ec814ac9ec19ece3
daigavane70 <daigavane70@gmail.com>
2021-05-18 20:21:16 +0530

    moreinfo infinite loop solved

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -61,8 +61,8 @@ const Events = (props) => {
             <button className={eventType!=='tech' ? 'tabs-btn active-tab' : 'tabs-btn'} onClick={changeEventType} id='non-tech-tab'>Non-Tech</button>
           </div>
           
-            {eventType ==='tech' ? techEvents.map( eve => {return(<div    key={eve.id} className='card-div'>  <Link to={`/events/${eve._id}`}><Card details = {eve} ></Card></Link> </div>)} ) : null}
-            {eventType !=='tech' ? nonTechEvents.map( eve => {return(<div key={eve.id} className='card-div'>  <Link to={`/events/${eve._id}`}><Card details = {eve} ></Card></Link> </div>)} ) : null}
+            {eventType ==='tech' ? techEvents.map( eve => {return(<div    key={eve._id} className='card-div'>  <Link to={`/events/${eve._id}`}><Card details = {eve} ></Card></Link> </div>)} ) : null}
+            {eventType !=='tech' ? nonTechEvents.map( eve => {return(<div key={eve._id} className='card-div'>  <Link to={`/events/${eve._id}`}><Card details = {eve} ></Card></Link> </div>)} ) : null}
       
       </div>
 
