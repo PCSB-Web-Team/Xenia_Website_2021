a9530d727ea7b1b7153d8035985baea00f088456
Atharva Belekar <athbelekar32@gmail.com>
2021-06-04 15:59:16 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/LandingPage/LandingPage";
 import Schedule from "./Components/Registrations/Registrations";
@@ -7,7 +6,6 @@ import EventDetail from "./Components/Events/MoreInfo/MoreInfo";
 import AboutUs from "./Components/AboutUs/About";
 import ContactUs from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
-import Cart from "./Components/Cart/MainCart";
 import Profile from "./Components/Profile/Profile";
 import WebTeam from "./Components/WebTeam/WebTeam";
 import Sponsors from "./Components/Sponsors/Sponsors";
@@ -44,37 +42,26 @@ const App = (props) => {
     <div className="Xenia" id="Xenia">
       <Navbar />
       <Background />
+
       <Switch>
         <Route exact path="/schedule">
-          {" "}
           <Schedule />
-          <Footer />{" "}
-        </Route>
-        <Route exact path="/events/:id">
-          {" "}
-          <EventDetail />
-          <Footer />{" "}
-        </Route>
-        <Route exact path="/events">
-          {" "}
-          <Events />
-          <Footer />{" "}
-        </Route>
-        <Route exact path="/side-events">
-          {" "}
-          <SideEvents />
-          <Footer />{" "}
+          <Footer />
         </Route>
 
-        <Route exact path="/cart">
-          {props.login ? (
-            <div>
-              <Cart />
-              <Footer />
-            </div>
-          ) : (
-            <Redirect to="/"></Redirect>
-          )}
+        <Route exact path="/events">
+          <Events />
+          <Footer />
+        </Route>
+
+        <Route exact path="/events/:id">
+          <EventDetail />
+          <Footer />
+        </Route>
+
+        <Route exact path="/side-events">
+          <SideEvents />
+          <Footer />
         </Route>
 
         <Route exact path="/profile">
@@ -89,18 +76,16 @@ const App = (props) => {
         </Route>
 
         <Route exact path="/sponsors">
-          {" "}
-          <Sponsors />
           <AboutUs />
+          <Sponsors />
           <ContactUs />
           <FAQ />
-          <Footer />{" "}
+          <Footer />
         </Route>
 
         <Route exact path="/webteam">
-          {" "}
           <WebTeam />
-          <Footer />{" "}
+          <Footer />
         </Route>
 
         <Route exact path="/forgot-password">
@@ -112,132 +97,13 @@ const App = (props) => {
         </Route>
 
         <Route exact path="/">
-          {" "}
-          <Home />{" "}
+          <Home />
         </Route>
+
         <Route component={NotFound} />
       </Switch>
     </div>
   );
-=======
-import Navbar from './Components/Navbar/Navbar';
-import Home from './Components/LandingPage/LandingPage';
-import Schedule from './Components/Registrations/Registrations';
-import Events from './Components/Events/Events';
-import EventDetail from './Components/Events/MoreInfo/MoreInfo';
-import AboutUs from './Components/AboutUs/About';
-import ContactUs from './Components/Contact/Contact';
-import Footer from './Components/Footer/Footer';
-import Profile from './Components/Profile/Profile';
-import WebTeam from './Components/WebTeam/WebTeam';
-import Sponsors from './Components/Sponsors/Sponsors';
-import { getLoggedInUser } from './Components/Config/api/User';
-import NotFound from './Components/404/404';
-import SideEvents from './Components/SideEvents/SideEvents';
-import FAQ from './Components/Contact/FAQ/FAQ';
-import Background from './Components/BackGround/Background';
-import ForgotPassword from './Components/ForgotPassword/forgot';
-import ResetPassword from './Components/ForgotPassword/reset';
-
-import React, { useEffect } from 'react';
-import {
-	Switch,
-	Route,
-	Redirect
-} from 'react-router-dom';
-import { getEventData, loggedIn, storeToken } from './Store/Actions';
-import { connect } from 'react-redux';
-import './App.css';
-
-const App = (props) => {
-
-	useEffect(() => {
-
-		const getUserData = async () => {
-
-			let userToken = localStorage.getItem('xeniaUserToken');
-
-			const res = await getLoggedInUser(userToken);
-
-			props.storeToken(userToken);
-
-			props.loggedIn(res.data.data);
-			
-		};
-
-		getUserData();
-
-	}, []);
-
-	return (
-		<div className="Xenia" id="Xenia">
-			<Navbar />
-			<Background />
-			
-			<Switch>
-				
-				<Route exact path="/schedule">
-					<Schedule />
-					<Footer />
-				</Route>
-				
-				<Route exact path="/events">
-					<Events />
-					<Footer />
-				</Route>
-				
-				<Route exact path="/events/:id">
-					<EventDetail />
-					<Footer />
-				</Route>
-
-				<Route exact path="/side-events">
-					<SideEvents />
-					<Footer />
-				</Route>
-				
-				<Route exact path="/profile">
-					{props.login ?
-						<div>
-							<Profile />
-							<Footer />
-						</div>
-						:
-						<Redirect to='/'></Redirect>
-					}
-				</Route>
-
-				<Route exact path="/sponsors">
-					<AboutUs/>
-					<Sponsors />
-					<ContactUs />
-					<FAQ/>
-					<Footer />
-				</Route>
-				
-				<Route exact path="/webteam">
-					<WebTeam />
-					<Footer />
-				</Route>
-				
-				<Route exact path="/forgot-password">
-					<ForgotPassword/>
-				</Route>
-
-				<Route exact path="/reset-password">
-					<ResetPassword/>
-				</Route>
-
-				<Route exact path="/">
-					<Home />
-				</Route>
-
-				<Route component={NotFound} />
-
-			</Switch>
-		</div>
-	);
->>>>>>> origin/newMain
 };
 
 const mapStatesToProps = (state) => {
