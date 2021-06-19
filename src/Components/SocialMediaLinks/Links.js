f4d3bab9d809733c00d760d440d47aef5e37ceb5
daigavane70 <daigavane70@gmail.com>
2021-06-19 09:28:26 +0530

    CSI added to secial media panel

--- src/Components/SocialMediaLinks/Links.js
+++ src/Components/SocialMediaLinks/Links.js
@@ -1,9 +1,11 @@
 import './Links.css';
+import CSILogo from '../../Assets/Images/csi.png';
 
 const SocialMedia = () => {
     return(
         <div className='social-links-panel'>
             <div className='panel-container'>
+                <div><a href='https://www.pictcsi.com/' target='_blanck' className='csi-logo-social'><img src={CSILogo}/></a></div>
                 <div><a href='http://www.facebook.com/csipict' target='_blanck' className='fb-logo'><i className='fa fa-facebook fa-lg'/></a></div>
                 <div><a href='http://www.instagram.com/csipict' target='_blanck' className='ig-logo'><i className='fa fa-instagram fa-lg'/></a></div>
                 <div><a href='http://www.linkedin.com/company/pict-csi' target='_blanck' className='in-logo'><i className='fa fa-linkedin fa-lg'/></a></div>
